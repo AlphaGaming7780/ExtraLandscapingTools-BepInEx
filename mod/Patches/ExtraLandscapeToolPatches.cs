ad29926e7d3270739b7e66512c03d247e589c2b5
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-03-06 18:18:19 +0100

    More Snap option

--- mod/Patches/ExtraLandscapeToolPatches.cs
+++ mod/Patches/ExtraLandscapeToolPatches.cs
@@ -76,7 +76,7 @@ namespace ExtraLandscapingTools.Patches
 
 		public static readonly string COUIBaseLocation = $"coui://{IconsResourceKey}";
 
-        internal readonly static List<string> pathToIconToLoad = [Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)];
+		internal readonly static List<string> pathToIconToLoad = [Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)];
 
 
 		static void Prefix(GameManager __instance)
@@ -178,75 +178,6 @@ namespace ExtraLandscapingTools.Patches
 			ELT_UI.SetUpMarker(prefab);
 		}
 	}
-
-    // [HarmonyPatch(typeof(ObjectToolSystem), nameof(ObjectToolSystem.GetAvailableSnapMask),
-    //     new Type[] { typeof(PlaceableObjectData), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(Snap), typeof(Snap) },
-    //     new ArgumentType[] {ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out })]
-	// class ObjectToolSystem_GetAvailableSnapMask
-	// {
-	// 	private static bool Prefix(PlaceableObjectData prefabPlaceableData, bool editorMode, bool isBuilding, bool isAssetStamp, bool brushing, bool stamping, out Snap onMask, out Snap offMask) {
-	// 		onMask = Snap.Upright;
-	// 		offMask = Snap.None;
-	// 		if ((prefabPlaceableData.m_Flags & Game.Objects.PlacementFlags.OwnerSide) != Game.Objects.PlacementFlags.None)
-	// 		{
-	// 			onMask |= Snap.OwnerSide;
-	// 		}
-	// 		else if ((prefabPlaceableData.m_Flags & (Game.Objects.PlacementFlags.RoadSide | Game.Objects.PlacementFlags.Shoreline | Game.Objects.PlacementFlags.Floating | Game.Objects.PlacementFlags.Hovering)) != Game.Objects.PlacementFlags.None)
-	// 		{
-	// 			if ((prefabPlaceableData.m_Flags & Game.Objects.PlacementFlags.RoadSide) != Game.Objects.PlacementFlags.None)
-	// 			{
-	// 				onMask |= Snap.NetSide;
-	// 				offMask |= Snap.NetSide;
-	// 			}
-	// 			if ((prefabPlaceableData.m_Flags & Game.Objects.PlacementFlags.Shoreline) != Game.Objects.PlacementFlags.None)
-	// 			{
-	// 				onMask |= Snap.Shoreline;
-	// 				offMask |= Snap.Shoreline;
-	// 			}
-	// 			if ((prefabPlaceableData.m_Flags & Game.Objects.PlacementFlags.Hovering) != Game.Objects.PlacementFlags.None)
-	// 			{
-	// 				onMask |= Snap.ObjectSurface;
-	// 				offMask |= Snap.ObjectSurface;
-	// 			}
-	// 		}
-	// 		else if ((prefabPlaceableData.m_Flags & (Game.Objects.PlacementFlags.RoadNode | Game.Objects.PlacementFlags.RoadEdge)) != Game.Objects.PlacementFlags.None)
-	// 		{
-	// 			if ((prefabPlaceableData.m_Flags & Game.Objects.PlacementFlags.RoadNode) != Game.Objects.PlacementFlags.None)
-	// 			{
-	// 				onMask |= Snap.NetNode;
-	// 			}
-	// 			if ((prefabPlaceableData.m_Flags & Game.Objects.PlacementFlags.RoadEdge) != Game.Objects.PlacementFlags.None)
-	// 			{
-	// 				onMask |= Snap.NetArea;
-	// 			}
-	// 		}
-	// 		else if (!isBuilding)
-	// 		{
-	// 			onMask |= Snap.ObjectSurface;
-	// 			offMask |= Snap.ObjectSurface;
-	// 			offMask |= Snap.Upright;
-	// 		}
-	// 		if ( editorMode && (!isAssetStamp || stamping))
-	// 		{
-	// 			onMask |= Snap.AutoParent;
-	// 			offMask |= Snap.AutoParent;
-	// 		}
-	// 		if (brushing)
-	// 		{
-	// 			onMask &= Snap.Upright;
-	// 			offMask &= Snap.Upright;
-	// 			onMask |= Snap.PrefabType;
-	// 			offMask |= Snap.PrefabType;
-	// 		}
-	// 		if (isBuilding || isAssetStamp)
-	// 		{
-	// 			onMask |= Snap.ContourLines;
-	// 			offMask |= Snap.ContourLines;
-	// 		}
-
-	// 		return false;
-	// 	}
-	// }
 
 	[HarmonyPatch(typeof(PrefabSystem), "OnCreate")]
 	public class PrefabSystem_OnCreate
