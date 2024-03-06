bd2f2e1cbf7af726b2491e7fca98982a25ffd73d
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-03-06 21:23:46 +0100

    Update ToolsSnapMask.cs

--- mod/Patches/ToolsSnapMask.cs
+++ mod/Patches/ToolsSnapMask.cs
@@ -156,4 +156,58 @@ public class ToolsSnapMask
 		}
 	}
 
+	[HarmonyPatch(typeof(AreaToolSystem), nameof(AreaToolSystem.GetAvailableSnapMask),
+		new Type[] { typeof(AreaGeometryData), typeof(bool), typeof(Snap), typeof(Snap) },
+		new ArgumentType[] {ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out })]
+	class AreaToolSystem_GetAvailableSnapMask
+	{
+		private static bool Prefix(AreaGeometryData prefabAreaData, bool editorMode, out Snap onMask, out Snap offMask) {
+
+			onMask = (Snap.ExistingGeometry | Snap.StraightDirection);
+			offMask = onMask;
+			switch (prefabAreaData.m_Type)
+			{
+			case Game.Areas.AreaType.Lot:
+				onMask |= Snap.NetSide | Snap.ObjectSide | Snap.LotGrid;
+				offMask |= Snap.NetSide | Snap.ObjectSide | Snap.LotGrid;
+				if (editorMode)
+				{
+					onMask |= Snap.AutoParent;
+					offMask |= Snap.AutoParent;
+					return false;
+				}
+				break;
+			case Game.Areas.AreaType.District:
+				onMask |= Snap.NetMiddle;
+				offMask |= Snap.NetMiddle;
+				return false;
+			case Game.Areas.AreaType.MapTile:
+				break;
+			case Game.Areas.AreaType.Space:
+				onMask |= Snap.NetSide | Snap.ObjectSide | Snap.ObjectSurface | Snap.LotGrid;
+				offMask |= Snap.NetSide | Snap.ObjectSide | Snap.ObjectSurface | Snap.LotGrid;
+				if (editorMode)
+				{
+					onMask |= Snap.AutoParent;
+					offMask |= Snap.AutoParent;
+					return false;
+				}
+				break;
+			case Game.Areas.AreaType.Surface:
+				onMask |= Snap.NetSide | Snap.ObjectSide | Snap.LotGrid;
+				offMask |= Snap.NetSide | Snap.ObjectSide | Snap.LotGrid;
+				if (editorMode)
+				{
+					onMask |= Snap.AutoParent;
+					offMask |= Snap.AutoParent;
+				}
+				break;
+			default:
+				return false;
+			}
+			return false;
+		}
+	}
+
+
 }
